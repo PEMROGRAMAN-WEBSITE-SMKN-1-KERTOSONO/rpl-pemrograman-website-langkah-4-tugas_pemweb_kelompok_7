cfa380f6db0dc05575ac99a5661e34647847880c
srmrau <pujirahayuayu815@gmail.com>
2026-05-20 14:16:21 +0700

    Menyelesaikan langkah 1

---
+++ nadia.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Input Data Catatan</title>
+</head>
+<body>
+
+    <h2>Form Input Catatan</h2>
+
+    <form action="" method="POST">
+
+        <label>Judul Catatan</label><br>
+        <input type="text" name="judul"><br><br>
+
+        <label>Isi Catatan</label><br>
+        <textarea name="isi_catatan"></textarea><br><br>
+
+        <label>Tanggal</label><br>
+        <input type="date" name="tanggal"><br><br>
+
+        <button type="submit" name="simpan">Simpan Catatan</button>
+
+    </form>
+
+</body>
+</html>
